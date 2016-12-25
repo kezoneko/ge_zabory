cc0ebe26c65f81d817fab8e34985a0c4e3854303
kezoneko <kezoneko@gmail.com>
2016-12-26 04:55:55 +0900

    оптимизация кода, добавлены функции маски, еще функции обработки данных

--- js/common.js
+++ js/common.js
@@ -1,22 +1,27 @@
-//Слайдер после заголовка
-var text_slide_cur=0;
-function showtext_slide(){
-	$('#text-slide'+(text_slide_cur+1)).css({opacity: 0}).animate({opacity: 1.0,left: "0"}, 1000);
-	setTimeout(hidetext_slide, 4000);
-}
-function hidetext_slide(){
-	$('#text-slide'+(text_slide_cur+1)).css({opacity: 1}).animate({opacity: 0,right: "0"}, 1000,function(){showtext_slide();});
-	text_slide_cur=(text_slide_cur+1)%2;
-}
-
 $(document).ready(function() {
+	//Слайдер после заголовка
+	var text_slide_cur=0;
+	function showtext_slide(){
+		$('#text-slide'+(text_slide_cur+1)).css({opacity: 0}).animate({opacity: 1.0,left: "0"}, 1000);
+		setTimeout(hidetext_slide, 4000);
+	}
+	function hidetext_slide(){
+		$('#text-slide'+(text_slide_cur+1)).css({opacity: 1}).animate({opacity: 0,right: "0"}, 1000,function(){showtext_slide();});
+		text_slide_cur=(text_slide_cur+1)%2;
+	}
 
 	//Слайдер после заголовка
 	showtext_slide();
 
-	$(".auth_buttons").click(function() {
-		$(this).next().slideToggle();
-	});
+	//Код jQuery, установливающий маску для ввода телефона элементу input
+	//1. После загрузки страницы,  когда все элементы будут доступны выполнить...
+	/*$(function(){
+	//2. Получить элемент, к которому необходимо добавить маску
+	$("#phone").mask("8(999) 999-9999");
+	});*/
+	//2. Получить элемент, к которому необходимо добавить маску
+	$(".phone").mask("+7 (999) 999-99-99");
+
 	$(".main_mnu_button").click(function() {
 		$(".main_mnu ul").slideToggle();
 	});
@@ -91,13 +96,24 @@ $(document).ready(function() {
 	
 	//Аякс отправка форм
 	//Документация: http://api.jquery.com/jquery.ajax/
+	
+	$("#formcost").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "callback.php",
+			data: $("#formcost").serialize()
+		}).done(function() {
+			alert("Спасибо за оставленную заявку! \nСпециалист сейчас с вами свяжется");
+		});
+		return false;
+	});
 	$("#callback").submit(function() {
 		$.ajax({
 			type: "GET",
-			url: "callback.php", /* url: "mail.php", */
+			url: "callback.php",
 			data: $("#callback").serialize()
 		}).done(function() {
-			alert("Спасибо за оставленную заявку!<br>Специалист сейчас с вами свяжется");
+			alert("Спасибо за оставленную заявку! \nСпециалист сейчас с вами свяжется");
 			setTimeout(function() {
 				$.fancybox.close();
 			}, 1000);
@@ -107,10 +123,10 @@ $(document).ready(function() {
 	$("#cost").submit(function() {
 		$.ajax({
 			type: "GET",
-			url: "#", /* url: "mail.php", */
+			url: "callback.php",
 			data: $("#cost").serialize()
 		}).done(function() {
-			alert("Спасибо за оставленную заявку!<br>Специалист сейчас с вами свяжется");
+			alert("Спасибо за оставленную заявку! \nСпециалист сейчас с вами свяжется");
 			setTimeout(function() {
 				$.fancybox.close();
 			}, 1000);
@@ -120,15 +136,14 @@ $(document).ready(function() {
 	$("#calculator").submit(function() {
 		$.ajax({
 			type: "GET",
-			url: "#", /* url: "mail.php", */
+			url: "callback.php",
 			data: $("#calculator").serialize()
 		}).done(function() {
-			alert("Спасибо за оставленную заявку!<br>Специалист сейчас с вами свяжется");
+			alert("Спасибо за оставленную заявку! \nСпециалист сейчас с вами свяжется");
 			setTimeout(function() {
 				$.fancybox.close();
 			}, 1000);
 		});
 		return false;
 	});
-
 });
