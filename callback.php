eadf87840cb490faa816405f5756e6f78b7aa7d3
kezoneko <kezoneko@gmail.com>
2016-12-26 04:56:21 +0900

    изменения для работы с данными

--- callback.php
+++ callback.php
@@ -1,13 +1,14 @@
 <?php
 
-$recepient = "youmail@ya.ru";
-$sitename = "Название сайта";
+$recepient = "kezoneko@gmail.com";
+$sitename = "Первая линия";
 
 $name = trim($_GET["name"]);
+$theme = trim($_GET["theme"]);
 $phone = trim($_GET["phone"]);
+$message = trim($_GET["message"]);
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone";
-//mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
-echo $message;
+$pagetitle = "Новая заявка с сайта \"$sitename\" - $theme";
+$message = "Из формы: $theme \nИмя: $name \nТелефон: $phone \nСообщение: \n $message";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
 ?>
